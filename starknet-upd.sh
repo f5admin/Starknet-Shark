362b24afd4eb3fa47370303a424333d79314b3ee
F5 Nodes <94526481+f5nodes@users.noreply.github.com>
2022-05-26 22:58:35 +0200

    init

---
+++ starknet-upd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ~/pathfinder
+git fetch
+git checkout v0.1.11-alpha
+cargo build --release --bin pathfinder
+mv ~/pathfinder/target/release/pathfinder /usr/local/bin/
+cd py
+source .venv/bin/activate
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
+systemctl restart starknetd
+pathfinder -V
